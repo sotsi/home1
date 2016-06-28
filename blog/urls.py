0a78946a603cfd503250b69a9a2d52481bcb5f05
sotsi <so.tsivras@gmail.com>
2016-06-28 12:40:29 +0300

    Contact form added...

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),
 	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+	#url(r'^quest/(?P<pk>\d+)/$', views.quest_detail, name='quest_detail'),
 	url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^quest/new/$', views.quest_new, name='quest_new'),
 	url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+	url(r'^quest/new/$', views.quest_edit1, name='quest_edit1'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from .models import Quest
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
+from .forms1 import ContactForm
 from django.shortcuts import redirect
+
+posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	#posts = Post.objects.raw('SELECT * FROM Post')
-	#for posts_trim in posts:
-	#	posts_t=posts_trim.text
-	#	posts_t=posts_t[:10]
 	return render(request, 'blog/post_list.html', {'posts': posts})
 	
 def post_detail(request, pk):
@@ -20,7 +20,7 @@ def post_detail(request, pk):
 def post_new(request):
 	form = PostForm()
 	return render(request, 'blog/post_edit.html', {'form': form})
-	
+
 def post_new(request):
 	if request.method == "POST":
 		form = PostForm(request.POST)
@@ -33,6 +33,25 @@ def post_new(request):
 	else:
 		form = PostForm()
 	return render(request, 'blog/post_edit.html', {'form': form})
+	
+def quest_new(request):
+	form = ContactForm()
+	return render(request, 'blog/quest_edit.html', {'form': form})
+	
+def quest_edit1(request):
+	form = ContactForm()
+	return render(request, 'blog/quest_edit1.html', {'form': form})
+	
+def quest_new(request):
+	if request.method == "POST":
+		form = ContactForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.save()
+			return render(request, 'blog/quest_edit1.html')
+	else:
+		form = ContactForm()
+	return render(request, 'blog/quest_edit.html', {'form': form})
 	
 def post_edit(request, pk):
 	post = get_object_or_404(Post, pk=pk)
